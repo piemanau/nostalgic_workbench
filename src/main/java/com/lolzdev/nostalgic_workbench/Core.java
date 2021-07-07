17a7985c0cb6bbc7012b9c080d14986faf25a7d7
LolzDEV <lollotorres06@gmail.com>
2021-07-07 22:00:49 +0200

    First commit

---
+++ src/main/java/com/lolzdev/nostalgic_workbench/Core.java
@@ -0,0 +1,10 @@
+package com.lolzdev.nostalgic_workbench;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Core implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
